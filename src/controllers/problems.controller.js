3537f78e1176acd64bc0ca924513b2ef124122c8
prashannjeetHexan <prasannjeet.p@hexantech.com>
2025-12-02 18:02:39 +0530

    Feat:Controller and routes

--- src/controllers/problems.controller.js
+++ src/controllers/problems.controller.js
@@ -2,6 +2,21 @@ function check(req, res) {
   return res.json({ message: "Checking controller" });
 }
 
+function getProblems(req, res) {}
+
+function getProblem(req, res) {}
+
+function addProblem(req, res) {}
+
+function updateProblem(req, res) {}
+
+function deleteProblem(req, res) {}
+
 module.exports = {
   check,
+  getProblem,
+  getProblems,
+  addProblem,
+  updateProblem,
+  deleteProblem,
 };
--- src/routes/v1/problem.routes.js
+++ src/routes/v1/problem.routes.js
@@ -6,4 +6,14 @@ const problemRoutes = express.Router();
 
 problemRoutes.get("/check", ProblemController.check);
 
+problemRoutes.get("/:id", ProblemController.getProblem);
+
+problemRoutes.get("/", ProblemController.getProblems);
+
+problemRoutes.post("/", ProblemController.addProblem);
+
+problemRoutes.put("/:id", ProblemController.updateProblem);
+
+problemRoutes.delete("/:id", ProblemController.deleteProblem);
+
 module.exports = problemRoutes;
